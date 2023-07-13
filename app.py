104501538d0a6c56513f9835fdd4f9449bb32a49
sabrinatiarab <sabrinatiarabachtiar@gmail.com>
2023-07-13 20:54:24 +0700

    New

--- app.py
+++ app.py
@@ -1,66 +1,29 @@
-# Import necessary libraries and packages 
-
-from flask import Flask, render_template, request # Flask object
+from flask import Flask, render_template, request
 import pandas as pd
-import joblib # package to load the model.pkl
+import joblib
 
-# Instantiate Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
-# Map URL to function
 @app.route("/")
-
-# Index function mapping to index.html
 def index():
-    return render_template('/index.html')
-    
-# Map predict.html to predict function
-@app.route('/predict', methods=['GET','POST'])
+    return render_template("index.html")
 
-# Predict function receives user input as dictionary
-# Calls another function to predict the student's output
-# Sends the result user
+@app.route("/predict", methods=["POST"])
 def predict():
-    
     if request.method == "POST":
-        #Get input data
-        to_predict_list = request.form.to_dict() 
-        
-        #Call preprocessDataAndPredict function and pass inputs
+        to_predict_list = request.form.to_dict()
         try:
             prediction = preprocessDataAndPredict(to_predict_list)
-
-            #Pass prediction to prediction template
-            return render_template('/predict.html', prediction = prediction)
-   
+            return render_template("predict.html", prediction=prediction)
         except ValueError:
-            return "Please Enter valid values"
-  
-        pass
-    pass
-
-# preprocessDataAndPredict function recieves the input
-# converts input to dataframe
-# makes the prediction using trained model
-# returns prediction results
+            return "Please enter valid values"
 
 def preprocessDataAndPredict(feature_dict):
-    
-    # Convert input to dataframe
-    test_data = {k:[v] for k,v in feature_dict.items()}  
+    test_data = {k: [v] for k, v in feature_dict.items()}
     test_data = pd.DataFrame(test_data)
-
-    # Open trained model
-    file = open("lr_model.pkl","rb")
-    
-    # Load trained model
-    trained_model = joblib.load(file)
-    
-    # Get prediction results
+    trained_model = joblib.load("lr_model.pkl")
     predict = trained_model.predict(test_data)
-
     return predict
-    
-    pass
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
