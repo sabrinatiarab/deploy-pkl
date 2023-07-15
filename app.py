3d756954acc1e4aae21d1f2d10b9780f8259d223
sabrinatiarab <sabrinatiarabachtiar@gmail.com>
2023-07-15 21:41:27 +0700

    New

--- app.py
+++ app.py
@@ -15,31 +15,17 @@ def predict():
         try:
             prediction = preprocessDataAndPredict(to_predict_list)
             return render_template("predict.html", prediction=prediction)
-        except ValueError:
-            return "Please enter valid values"
+        except ValueError as ve:
+            return f"ValueError: {str(ve)}, please enter valid values"
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
 def preprocessDataAndPredict(feature_dict):
-    test_data = {k: [v] for k, v in feature_dict.items()}
+    test_data = {k.replace(' ', '_').replace('-', '_'): [v] for k, v in feature_dict.items()}
     test_data = pd.DataFrame(test_data)
     trained_model = joblib.load("lr_model.pkl")
     predict = trained_model.predict(test_data)
-    
-    if predict == 0:
-        prediction = "Class 0"
-    elif predict == 1:
-        prediction = "Class 1"
-    elif predict == 2:
-        prediction = "Class 2"
-    elif predict == 3:
-        prediction = "Class 3"
-    elif predict == 4:
-        prediction = "Class 4"
-    else:
-        prediction = "Class 5"
-    
-    return prediction
+    return predict
 
 if __name__ == "__main__":
     app.run(debug=True)
